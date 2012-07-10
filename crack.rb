82a3016ed83dd54f98667c34adfda5bed4295704
Lari Lehtomäki <lari@lehtomaki.fi>
2012-07-10 23:25:36 +0300

    Working complex key cracker

--- crack.rb
+++ crack.rb
@@ -1,33 +1,46 @@
-
 word = ARGV[0].downcase.strip
 
 if !ARGV[1].nil? then
 	pattern = ARGV[1].downcase.strip
 end
 
+def incr_and_print(ciphertext,array,index,pattern)
+	while array[index] <= 25 do
+		crack(ciphertext,array,pattern)
+		if index > 0 
+			incr_and_print(ciphertext,array,index-1,pattern)
+		end
+		array[index] = array[index] + 1
+	end
+	array[index] = 0
+end
+
+def crack (ciphertext,keyarray,pattern)
+	guess=''
+	x=0
+	ciphertext.bytes do |p|
+		if p != 32 then
+			guess.concat  (( p - 97 + keyarray[x] ) % 26 ) + 97 
+			x = ( x + 1 ) % keyarray.size
+		else
+			guess.concat ' '
+		end
+	end
+	if !pattern.nil? then
+		if guess.include? pattern then
+			puts "+#{keyarray} #{guess}"
+		end
+	else
+		puts "+#{keyarray} #{guess}"
+	end	
+end
+
 ##
-## Simple Ceasar
+## Complex Ceasar
 ##
 
 add = [0]
-i=0
-while add[i] <= 25 do
-  guess=''
-  x=0
-  word.bytes do |p|
-    if p != 32 then
-      guess.concat  (( p - 97 + add[x] ) % 26 ) + 97 
-      x = ( x + 1 ) % add.size
-    else
-      guess.concat ' '
-    end
-  end
-  if !pattern.nil? then
-    if guess.include? pattern then
-      puts "+#{add} #{guess}"
-    end
-  else
-    puts "+#{add} #{guess}"
-  end
-  add[i] = add[i] + 1
-end
+i=add.size-1
+
+incr_and_print(word,add,i,pattern)
+
